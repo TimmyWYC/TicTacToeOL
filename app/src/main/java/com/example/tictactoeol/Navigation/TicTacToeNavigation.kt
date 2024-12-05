bd9695c47a40c54774f51e864ea5b628ffdcb34b
TimmyWYC <54103945+TimmyWYC@users.noreply.github.com>
2024-12-05 01:54:13 +0100

    add a temp lobby screen

--- app/src/main/java/com/example/tictactoeol/Navigation/TicTacToeNavigation.kt
+++ app/src/main/java/com/example/tictactoeol/Navigation/TicTacToeNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tictactoeol.Screen.HomeScreen
+import com.example.tictactoeol.Screen.LobbyScreen
 
 @Composable
 fun TicTacToeNav(){
@@ -12,6 +13,7 @@ fun TicTacToeNav(){
 
     NavHost(navController = navController, startDestination = "Home") {
         composable("Home") { HomeScreen(navController)}
+        composable("Lobby") { LobbyScreen(navController)}
     }
 
 }
---
+++ app/src/main/java/com/example/tictactoeol/Screen/LobbyScreen.kt
@@ -0,0 +1,10 @@
+package com.example.tictactoeol.Screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun LobbyScreen(navController: NavController) {
+
+
+}
