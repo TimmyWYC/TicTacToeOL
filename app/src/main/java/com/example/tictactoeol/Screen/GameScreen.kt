9c363bd028500452fb6a51ff7d63179b52ea0264
TimmyWYC <54103945+TimmyWYC@users.noreply.github.com>
2024-12-08 02:37:52 +0100

    Remove unused import

--- app/src/main/java/com/example/tictactoeol/Screen/GameScreen.kt
+++ app/src/main/java/com/example/tictactoeol/Screen/GameScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
--- app/src/main/java/com/example/tictactoeol/Screen/HomeScreen.kt
+++ app/src/main/java/com/example/tictactoeol/Screen/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.example.tictactoeol.Screen
 
 import android.content.Context
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
